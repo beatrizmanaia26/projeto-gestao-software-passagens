6f921ec791e44de38c30fadef1465578d3cc2a17
Rafaela Altheman <134940501+RafaAltheman@users.noreply.github.com>
2026-03-13 23:49:42 +0000

    teste conexao com banco

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,25 @@
-require('dotenv').config({ path: __dirname + '/.env' });
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-const usersRoutes = require('./src/routes/usersRoutes');
-const tripsRoutes = require('./src/routes/tripsRoutes');
-const cartRoutes = require('./src/routes/cartRoutes');
-const ordersRoutes = require('./src/routes/ordersRoutes');
-const paymentsRoutes = require('./src/routes/paymentsRoutes');
+const usersRoutes = require('./src/routes/usersRoutes')
+const tripsRoutes = require('./src/routes/tripsRoutes')
+const cartRoutes = require('./src/routes/cartRoutes')
+const ordersRoutes = require('./src/routes/ordersRoutes')
+const paymentsRoutes = require('./src/routes/paymentsRoutes')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use('/users', usersRoutes);
-app.use('/trips', tripsRoutes);
-app.use('/cart', cartRoutes);
-app.use('/orders', ordersRoutes);
-app.use('/payments', paymentsRoutes);
+app.use('/api/users', usersRoutes)
+app.use('/api/trips', tripsRoutes)
+app.use('/api/cart', cartRoutes)
+app.use('/api/orders', ordersRoutes)
+app.use('/api/payments', paymentsRoutes)
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend funcionando' });
-});
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
-const supabase = require('./src/models/supabaseClient');
-
-app.get('/teste-users', async (req, res) => {
-  const { data, error } = await supabase
-    .from('users')
-    .select('id, name, email, cpf');
-
-  if (error) {
-    return res.status(400).json(error);
-  }
-
-  res.json(data);
-});
+  console.log(`Servidor rodando na porta ${PORT}`)
+})
